988b60641097c9dcce04662475945d3daa591d27
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2025-02-13 20:28:36 -0300

    deploy: page.tsx cambiado para deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { HeroUIProvider } from "@heroui/react";
 import { Suspense } from "react";
 import MatrixScreen from "../presentation/features/matrix/MatrixScreen";
 
 
 export default function Home() {
   return (
-    <HeroUIProvider>
       <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-slate-900 to-neutral-950">
         <h1 className="text-3xl font-bold mb-4 bg-gradient-to-r from-purple-400 to-pink-500 text-transparent bg-clip-text cursor-default select-none">
           A Peculiar Tetris Piece
@@ -19,6 +17,5 @@ export default function Home() {
 
 
       </main>
-    </HeroUIProvider>
   );
 }
