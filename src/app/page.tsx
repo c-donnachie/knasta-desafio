57e8ae96e6a51f1f5bc786f385689522d41da750
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2025-02-13 21:09:53 -0300

    fix: se arreglo estilos corrompidos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { HeroUIProvider } from "@heroui/react";
 import { Suspense } from "react";
 import MatrixScreen from "../presentation/features/matrix/MatrixScreen";
 
 
 export default function Home() {
   return (
+    <HeroUIProvider>
       <main className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-slate-900 to-neutral-950">
         <h1 className="text-3xl font-bold mb-4 bg-gradient-to-r from-purple-400 to-pink-500 text-transparent bg-clip-text cursor-default select-none">
           A Peculiar Tetris Piece
@@ -15,7 +17,7 @@ export default function Home() {
           <MatrixScreen />
         </Suspense>
 
-
       </main>
+    </HeroUIProvider>
   );
 }
